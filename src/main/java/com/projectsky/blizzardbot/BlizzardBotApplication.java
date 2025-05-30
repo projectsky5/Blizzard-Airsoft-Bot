a09dcfd83778d4acd354dc17a06770de7f46278b
projectsky5 <mackvej@gmail.com>
2025-05-30 15:08:25 +0300

    Initial commit

---
+++ src/main/java/com/projectsky/blizzardbot/BlizzardBotApplication.java
@@ -0,0 +1,13 @@
+package com.projectsky.blizzardbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlizzardBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlizzardBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projectsky/blizzardbot/BlizzardBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectsky.blizzardbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlizzardBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
